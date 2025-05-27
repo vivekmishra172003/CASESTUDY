b04ff8c260301402df98272e80773bd750b84344
Vivek Mishra <vivekmishra172003@gmail.com>
2025-05-27 07:59:08 +0530

    somw work is done

---
+++ Check.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Check{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		// int n = sc.nextInt();
+		Date d = new Date();
+		System.out.println(d.getDate());
+		
+	}
+
+}
